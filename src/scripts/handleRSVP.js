26bef1d02b629b859b603ac72c47406ab7f7b350
justinmitchell341 <justin.mitchell341@gmail.com>
2024-06-03 11:20:03 -0700

    Revert "Gotta fix"
    This reverts commit c4e4f2cd1d6dac2eb985297439124639b14d45b1.

--- src/scripts/handleRSVP.js
+++ src/scripts/handleRSVP.js
@@ -7,15 +7,14 @@ export default async function handleRsvpFormSubmission(event) {
     event.preventDefault(); // Prevent the default form submit behavior
     const formData = new FormData(form);
     const data = Object.fromEntries(formData); // Convert form data to an object
-    alert('Thank you, your RSVP has been submitted successfully!');
     try {
       // Add the RSVP data to the "rsvps" collection in Firestore
       const docRef = await addDoc(collection(db, "rsvps"), data);
       const successMessage = document.createElement('div');
       successMessage.innerText = 'Thank you, your RSVP has been submitted successfully!';
       form.appendChild(successMessage);
-      setTimeout(() => {
-        successMessage.remove();
+       setTimeout(() => {
+       successMessage.remove();
       }, 5000);
       console.log("RSVP submitted successfully, document ID: ", docRef.id);
       form.reset();
