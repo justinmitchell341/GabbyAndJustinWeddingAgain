bf9fee8b4f84e10157bb3829ecdd2ca8c149362b
justinmitchell341 <justin.mitchell341@gmail.com>
2024-06-20 21:22:49 -0700

    Front Page Styling Nearly Done

---
+++ src/scripts/countdown.js
@@ -0,0 +1,17 @@
+function updateCountdown() {
+    const weddingDate = new Date('September 7, 2024 15:00:00').getTime();
+    const now = new Date().getTime();
+    const distance = weddingDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+}
+
+setInterval(updateCountdown, 1000);
